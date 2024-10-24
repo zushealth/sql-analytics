4505a71ddb76980bf36075b05c7e0b6e619dda8f
NLuzarraga <93792086+NLuzarraga@users.noreply.github.com>
2024-10-24 17:04:10 -0500

    Update most_recent_email.sql

--- 2.Analyzing_Patient_Demographics/most_recent_email.sql
+++ 2.Analyzing_Patient_Demographics/most_recent_email.sql
@@ -1,61 +1,61 @@
 -- -- Query to identify both first-party emails and third-party emails
 WITH PROCESSED_EMAILS AS (
     SELECT
-        p.upid,
-        cp.use AS email_use,
-        LOWER(TRIM(REPLACE(cp.value, ' ', ''))) AS email_address,  -- Trim, remove spaces, and convert to lowercase
-        p.data_source,
+        p.UPID,
+        cp.USE AS email_use,
+        LOWER(TRIM(REPLACE(cp.VALUE, ' ', ''))) AS email_address,  -- Trim, remove spaces, and convert to lowercase
+        p.DATA_SOURCE,
         COALESCE(
-            encounter.period_end,
-            encounter.period_start, 
-            TRY_TO_TIMESTAMP(document_reference.period_end),
-            TRY_TO_TIMESTAMP(document_reference.period_start),
-            document_reference.document_date, 
-            document_reference.last_updated, 
-            p.last_updated
+            ENCOUNTER.PERIOD_END,
+            ENCOUNTER.PERIOD_START, 
+            TRY_TO_TIMESTAMP(DOCUMENT_REFERENCE.PERIOD_END),
+            TRY_TO_TIMESTAMP(DOCUMENT_REFERENCE.PERIOD_START),
+            DOCUMENT_REFERENCE.DOCUMENT_DATE, 
+            DOCUMENT_REFERENCE.LAST_UPDATED, 
+            p.LAST_UPDATED
         ) AS email_recorded_date
-    FROM patient p
-    JOIN patient_telecom pt ON p.id = pt.patient_id
-    JOIN contact_point cp ON pt.contact_point_id = cp.id
-    LEFT JOIN document_reference ON document_reference.subject_patient_id = p.id
-    LEFT JOIN encounter ON document_reference.encounter_id = encounter.id
-    WHERE cp.system = 'email'
-      AND cp.value REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'  -- Validate email format
-      AND NOT (email_address LIKE ANY ('%noemail%', '%none%', '%opt-out%', '%donotreply%', '%unsubscribe%', '%invalid%', '%test%'))
-      AND cp.value IS NOT NULL
-      AND cp.value != ''
+    FROM PATIENT p
+    JOIN PATIENT_TELECOM pt ON p.ID = pt.PATIENT_ID
+    JOIN CONTACT_POINT cp ON pt.CONTACT_POINT_ID = cp.ID
+    LEFT JOIN DOCUMENT_REFERENCE ON DOCUMENT_REFERENCE.SUBJECT_PATIENT_ID = p.ID
+    LEFT JOIN ENCOUNTER ON DOCUMENT_REFERENCE.ENCOUNTER_ID = ENCOUNTER.ID
+    WHERE cp.SYSTEM = 'email'
+      AND cp.VALUE REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'  -- Validate email format
+      AND NOT (EMAIL_ADDRESS LIKE ANY ('%noemail%', '%none%', '%opt-out%', '%donotreply%', '%unsubscribe%', '%invalid%', '%test%'))
+      AND cp.VALUE IS NOT NULL
+      AND cp.VALUE != ''
 
 )
 , FIRST_PARTY_EMAILS AS (
     SELECT 
-        upid,
-        ARRAY_UNIQUE_AGG(email_use) AS email_use,
-        ARRAY_UNIQUE_AGG(email_address) AS email_addresses
+        UPID,
+        ARRAY_UNIQUE_AGG(EMAIL_USE) AS email_use,
+        ARRAY_UNIQUE_AGG(EMAIL_ADDRESS) AS email_addresses
     FROM PROCESSED_EMAILS
-    WHERE data_source IS NULL
-    GROUP BY upid
+    WHERE DATA_SOURCE IS NULL
+    GROUP BY UPID
 ), 
 THIRD_PARTY_EMAILS AS (
     SELECT 
-        upid,
+        UPID,
         ARRAY_UNIQUE_AGG(email_use) AS email_use,
-        email_address,
-        email_recorded_date,
-        RANK() OVER (PARTITION BY upid, email_address ORDER BY email_recorded_date DESC NULLS LAST) AS recency_rank
+        EMAIL_ADDRESS,
+        EMAIL_RECORDED_DATE,
+        RANK() OVER (PARTITION BY UPID, EMAIL_ADDRESS ORDER BY EMAIL_RECORDED_DATE DESC NULLS LAST) AS recency_rank
     FROM PROCESSED_EMAILS
-    WHERE data_source IS NOT NULL
-    GROUP BY upid, email_address, email_recorded_date
+    WHERE DATA_SOURCE IS NOT NULL
+    GROUP BY UPID, EMAIL_ADDRESS, EMAIL_RECORDED_DATE
     QUALIFY recency_rank = 1
 )
 SELECT 
-    fp.upid AS upid,
-    tp.email_use AS third_party_email_use,
-    tp.email_address AS third_party_email_address,
-    tp.email_recorded_date AS third_party_email_recorded_date,
-    fp.email_use AS first_party_phone_use,
-    fp.email_addresses AS first_party_phone_numbers
+    fp.UPID AS UPID,
+    tp.EMAIL_USE AS third_party_email_use,
+    tp.EMAIL_ADDRESS AS third_party_email_address,
+    tp.EMAIL_RECORDED_DATE AS third_party_email_recorded_date,
+    fp.EMAIL_USE AS first_party_phone_use,
+    fp.EMAIL_ADDRESSES AS first_party_phone_numbers
 FROM THIRD_PARTY_EMAILS tp
 LEFT JOIN FIRST_PARTY_EMAILS fp
-ON fp.upid = tp.upid
-AND NOT ARRAY_CONTAINS(tp.email_address::variant, fp.email_addresses)
-ORDER BY fp.upid, tp.email_recorded_date DESC NULLS LAST;
+ON fp.UPID = tp.UPID
+AND NOT ARRAY_CONTAINS(tp.EMAIL_ADDRESS::variant, fp.email_addresses)
+ORDER BY fp.UPID, tp.EMAIL_RECORDED_DATE DESC NULLS LAST;
