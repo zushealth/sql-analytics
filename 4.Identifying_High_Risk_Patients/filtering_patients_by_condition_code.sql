6b1ddc5f04ed7f0e052def332beefcea15fce78a
NLuzarraga <93792086+NLuzarraga@users.noreply.github.com>
2024-10-24 17:44:43 -0500

    Create filtering_patients_by_condition_code.sql

---
+++ 4.Identifying_High_Risk_Patients/filtering_patients_by_condition_code.sql
@@ -0,0 +1,7 @@
+-- Query to filter for patients (UPIDs) with a record of SNOMED codes 73211009, 313436004, 44054006, 111552007, indicating that they’ve previously been diagnosed with diabetes mellitus
+SELECT 
+    DISTINCT UPID
+FROM
+    LENS_SNOMED_CONDITION
+WHERE 
+    CODE_SNOMED IN ('73211009','313436004','44054006','111552007');
