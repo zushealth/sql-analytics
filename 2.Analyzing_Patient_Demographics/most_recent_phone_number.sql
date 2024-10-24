675694805c662dedee84a6318b374706ff4684e8
NLuzarraga <93792086+NLuzarraga@users.noreply.github.com>
2024-10-24 16:18:53 -0500

    Update most_recent_phone_number.sql

--- 2.Analyzing_Patient_Demographics/most_recent_phone_number.sql
+++ 2.Analyzing_Patient_Demographics/most_recent_phone_number.sql
@@ -1,64 +1,65 @@
+-- Query to identify both first-party phone numbers and third-party phone numbers
 WITH PROCESSED_PHONE_NUMBERS AS (
     SELECT
-        p.upid,
-        cp.use AS phone_use,
-        REGEXP_REPLACE(REPLACE(cp.value, '+1', ''), '[^0-9]', '') AS phone_number,
+        p.UPID,
+        cp.USE AS phone_use,
+        REGEXP_REPLACE(REPLACE(cp.VALUE, '+1', ''), '[^0-9]', '') AS phone_number,
         CONCAT(
-            SUBSTR(REGEXP_REPLACE(REPLACE(cp.value, '+1', ''), '[^0-9]', ''), 1, 3),  -- Area code (XXX)
+            SUBSTR(REGEXP_REPLACE(REPLACE(cp.VALUE, '+1', ''), '[^0-9]', ''), 1, 3),  -- Area code (XXX)
             '-',
-            SUBSTR(REGEXP_REPLACE(REPLACE(cp.value, '+1', ''), '[^0-9]', ''), 4, 3),  -- First three digits (XXX)
+            SUBSTR(REGEXP_REPLACE(REPLACE(cp.VALUE, '+1', ''), '[^0-9]', ''), 4, 3),  -- First three digits (XXX)
             '-',
-            SUBSTR(REGEXP_REPLACE(REPLACE(cp.value, '+1', ''), '[^0-9]', ''), 7, 4)   -- Last four digits (XXXX)
+            SUBSTR(REGEXP_REPLACE(REPLACE(cp.VALUE, '+1', ''), '[^0-9]', ''), 7, 4)   -- Last four digits (XXXX)
         ) AS formatted_phone_number,
-        p.data_source,
+        p.DATA_SOURCE,
         COALESCE(
-            encounter.period_end,
-            encounter.period_start, 
-            TRY_TO_TIMESTAMP(document_reference.period_end),
-            TRY_TO_TIMESTAMP(document_reference.period_start),
-            document_reference.document_date, 
-            document_reference.last_updated, 
-            p.last_updated
+            ENCOUNTER.PERIOD_END,
+            ENCOUNTER.PERIOD_START, 
+            TRY_TO_TIMESTAMP(DOCUMENT_REFERENCE.PERIOD_END),
+            TRY_TO_TIMESTAMP(DOCUMENT_REFERENCE.PERIOD_START),
+            DOCUMENT_REFERENCE.DOCUMENT_DATE, 
+            DOCUMENT_REFERENCE.LAST_UPDATED, 
+            p.LAST_UPDATED
         ) AS phone_number_recorded_date
-    FROM patient p
-    JOIN patient_telecom pt ON p.id = pt.patient_id
-    JOIN contact_point cp ON pt.contact_point_id = cp.id
-    LEFT JOIN document_reference ON document_reference.subject_patient_id = p.id
-    LEFT JOIN encounter ON document_reference.encounter_id = encounter.id
-    WHERE cp.system = 'phone'
-      AND LENGTH(REGEXP_REPLACE(REPLACE(cp.value, '+1', ''), '[^0-9]', '')) = 10
-      AND NOT (REGEXP_REPLACE(REPLACE(cp.value, '+1', ''), '[^0-9]', '') LIKE ANY ('%0000%','%8888%','%5555%','%9999%')) 
+    FROM PATIENT p
+    JOIN PATIENT_TELECOM pt ON p.ID = pt.PATIENT_ID
+    JOIN CONTACT_POINT cp ON pt.CONTACT_POINT_ID = cp.ID
+    LEFT JOIN DOCUMENT_REFERENCE ON DOCUMENT_REFERENCE.SUBJECT_PATIENT_ID = p.ID
+    LEFT JOIN ENCOUNTER ON DOCUMENT_REFERENCE.ENCOUNTER_ID = ENCOUNTER.ID
+    WHERE cp.SYSTEM = 'phone'
+      AND LENGTH(REGEXP_REPLACE(REPLACE(cp.VALUE, '+1', ''), '[^0-9]', '')) = 10
+      AND NOT (REGEXP_REPLACE(REPLACE(cp.VALUE, '+1', ''), '[^0-9]', '') LIKE ANY ('%0000%','%8888%','%5555%','%9999%')) 
 )
 , FIRST_PARTY_PHONE_NUMBERS AS (
     SELECT 
-        upid,
-        ARRAY_UNIQUE_AGG(phone_use) AS phone_use,
-        ARRAY_UNIQUE_AGG(formatted_phone_number) AS phone_numbers
+        UPID,
+        ARRAY_UNIQUE_AGG(PHONE_USE) AS phone_use,
+        ARRAY_UNIQUE_AGG(FORMATTED_PHONE_NUMBER) AS phone_numbers
     FROM PROCESSED_PHONE_NUMBERS
-    WHERE data_source IS NULL
-    GROUP BY upid
+    WHERE DATA_SOURCE IS NULL
+    GROUP BY UPID
 ), 
 THIRD_PARTY_PHONE_NUMBERS AS (
     SELECT 
-        upid,
-        ARRAY_UNIQUE_AGG(phone_use) AS phone_use,
-        formatted_phone_number,
-        phone_number_recorded_date,
-        RANK() OVER (PARTITION BY upid, formatted_phone_number ORDER BY phone_number_recorded_date DESC NULLS LAST) AS recency_rank
+        UPID,
+        ARRAY_UNIQUE_AGG(PHONE_USE) AS phone_use,
+        FORMATTED_PHONE_NUMBER,
+        PHONE_NUMBER_RECORDED_DATE,
+        RANK() OVER (PARTITION BY UPID, FORMATTED_PHONE_NUMBER ORDER BY PHONE_NUMBER_RECORDED_DATE DESC NULLS LAST) AS recency_rank
     FROM PROCESSED_PHONE_NUMBERS
-    WHERE data_source IS NOT NULL
-    GROUP BY upid, formatted_phone_number, phone_number_recorded_date
+    WHERE DATA_SOURCE IS NOT NULL
+    GROUP BY UPID, FORMATTED_PHONE_NUMBER, PHONE_NUMBER_RECORDED_DATE
     QUALIFY recency_rank = 1
 )
 SELECT 
-    tp.upid AS upid,
-    tp.phone_use AS third_party_phone_use,
-    tp.formatted_phone_number AS third_party_phone_number,
-    tp.phone_number_recorded_date AS third_party_phone_recorded_date,
-    fp.phone_use AS first_party_phone_use,
-    fp.phone_numbers AS first_party_phone_numbers,
+    tp.UPID AS upid,
+    tp.PHONE_USE AS third_party_phone_use,
+    tp.FORMATTED_PHONE_NUMBER AS third_party_phone_number,
+    tp.PHONE_NUMBER_RECORDED_DATE AS third_party_phone_recorded_date,
+    fp.PHONE_USE AS first_party_phone_use,
+    fp.PHONE_NUMBERS AS first_party_phone_numbers,
 FROM THIRD_PARTY_PHONE_NUMBERS tp
 LEFT JOIN FIRST_PARTY_PHONE_NUMBERS fp
-ON fp.upid = tp.upid
-AND NOT ARRAY_CONTAINS(tp.formatted_phone_number::variant, fp.phone_numbers)
-ORDER BY tp.upid, tp.phone_number_recorded_date DESC NULLS LAST;
+ON fp.UPID = tp.UPID
+AND NOT ARRAY_CONTAINS(tp.FORMATTED_PHONE_NUMBER::variant, fp.phone_numbers)
+ORDER BY tp.UPID, tp.PHONE_NUMBER_RECORDED_DATE DESC NULLS LAST;
