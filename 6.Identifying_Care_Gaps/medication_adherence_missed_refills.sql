b7cb2c42278e1c7f638dd85481974e6c5031499a
NLuzarraga <93792086+NLuzarraga@users.noreply.github.com>
2024-10-25 09:42:47 -0500

    Update medication_adherence_missed_refills.sql

--- 6.Identifying_Care_Gaps/medication_adherence_missed_refills.sql
+++ 6.Identifying_Care_Gaps/medication_adherence_missed_refills.sql
@@ -1,30 +1,30 @@
 -- Query to identify patients with missed medication refills
 WITH unique_dispenses AS (
     SELECT DISTINCT
-        mr.upid,
+        mr.UPID,
         codes.VALUE:code::varchar AS rxnorm_active_ingredient,
-        mr.number_of_repeats_allowed,
-        md.when_prepared,
-        md.when_handed_over,
-        md.days_supply
-    FROM medication_dispense md
-    JOIN medication_request mr
-        ON mr.id = md.authorizing_prescription_medication_request_id
-    LATERAL FLATTEN(input => mr.resource_json:medicationCodeableConcept:coding) AS codes
+        mr.NUMBER_OF_REPEATS_ALLOWED,
+        md.WHEN_PREPARED,
+        md.WHEN_HANDED_OVER,
+        md.DAYS_SUPPLY
+    FROM MEDICATION_DISPENSE md
+    JOIN MEDICATION_REQUEST mr
+        ON mr.ID = md.AUTHORIZING_PRESCRIPTION_MEDICATION_REQUEST_ID,
+    LATERAL FLATTEN(input => mr.RESOURCE_JSON:medicationCodeableConcept:coding) AS codes,
     LATERAL FLATTEN(input => codes.VALUE:extension) AS codes_extension
-    WHERE mr.number_of_repeats_allowed IS NOT NULL
-      AND mr.number_of_repeats_allowed > 0
+    WHERE mr.NUMBER_OF_REPEATS_ALLOWED IS NOT NULL
+      AND mr.NUMBER_OF_REPEATS_ALLOWED > 0
       AND codes_extension.VALUE:valueString = 'ActiveIngredient'
 )
 SELECT
     *,
     ROW_NUMBER() OVER (
-        PARTITION BY upid, rxnorm_active_ingredient 
-        ORDER BY COALESCE(when_handed_over, when_prepared) DESC NULLS LAST
+        PARTITION BY UPID, rxnorm_active_ingredient 
+        ORDER BY COALESCE(WHEN_HANDED_OVER, WHEN_PREPARED) DESC NULLS LAST
     ) AS rnk,
-    DATEDIFF('day', COALESCE(when_handed_over, when_prepared), CURRENT_DATE()) AS days_since_last_dispense_or_fill
+    DATEDIFF('day', COALESCE(WHEN_HANDED_OVER, WHEN_PREPARED), CURRENT_DATE()) AS days_since_last_dispense_or_fill
 FROM unique_dispenses
 QUALIFY rnk = 1
-  AND DATEDIFF('day', COALESCE(when_handed_over, when_prepared), CURRENT_DATE()) > 
+  AND DATEDIFF('day', COALESCE(WHEN_HANDED_OVER, WHEN_PREPARED), CURRENT_DATE()) > 
       GREATEST(36, days_supply + 5)
-ORDER BY COALESCE(when_handed_over, when_prepared) DESC;
+ORDER BY COALESCE(WHEN_HANDED_OVER, WHEN_PREPARED) DESC;
