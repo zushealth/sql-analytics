4df9c8187eae7ebc8f573d6fcf061db88013c116
NLuzarraga <93792086+NLuzarraga@users.noreply.github.com>
2024-10-31 10:53:27 -0500

    Update hcc_recapture_opportunities.sql
    Updated logic to only surface third-party HCCs captured in the past 2 years, to increase the likelihood that the diagnosis is still appropriate and relevant

--- 5.Supporting_Risk_Capture/hcc_recapture_opportunities.sql
+++ 5.Supporting_Risk_Capture/hcc_recapture_opportunities.sql
@@ -1,5 +1,4 @@
-
--- Step 1: Identify HCC codes found by first-party and the third-party sources for each patient (UPID)
+-- Step 1: For each patient (UPID), identify HCC codes found by first-party sources and by third-party sources in the past 2 years
 WITH FIRST_PARTY_HCCS AS (
     SELECT
         UPID,
@@ -12,7 +11,7 @@ WITH FIRST_PARTY_HCCS AS (
     GROUP BY 
         UPID,
         CODE_HCC
-), THIRD_PARTY_HCCS AS (
+), RECENT_THIRD_PARTY_HCCS AS (
     SELECT
         UPID,
         CODE_HCC
@@ -21,60 +20,63 @@ WITH FIRST_PARTY_HCCS AS (
     WHERE
         DATA_SOURCE IS NOT NULL
         AND CODE_HCC IS NOT NULL
+        AND RECORDED_DATE >= DATEADD('year', -2, CURRENT_DATE)
     GROUP BY 
         UPID,
         CODE_HCC
 ),
--- Step 2: Identify HCC codes that are found only by third-party sources for each patient
-THIRD_PARTY_ONLY_HCCS AS (
+-- Step 2: Identify HCC codes that are found only by third-party sources within the last 2 years for each patient
+RECENT_THIRD_PARTY_ONLY_HCCS AS (
     SELECT
         tp.UPID,
         tp.CODE_HCC,
     FROM
-        THIRD_PARTY_HCCS tp
+        RECENT_THIRD_PARTY_HCCS tp
     LEFT JOIN FIRST_PARTY_HCCS fp
         ON tp.UPID = fp.UPID 
         AND tp.CODE_HCC = fp.CODE_HCC
     WHERE
         fp.CODE_HCC IS NULL
 ),
--- Step 3: Count distinct HCCs per patient from both third and first-party sources
+-- Step 3: Count distinct HCCs per patient from both first and third-party sources
 PATIENT_HCC_COUNTS AS (
     SELECT
         UPID,
         COUNT(DISTINCT CASE WHEN DATA_SOURCE IS NULL THEN CODE_HCC END) AS FIRST_PARTY_HCC_COUNT,
-        COUNT(DISTINCT CASE WHEN DATA_SOURCE IS NOT NULL THEN CODE_HCC END) AS THIRD_PARTY_HCC_COUNT
+        COUNT(DISTINCT CASE WHEN DATA_SOURCE IS NOT NULL 
+            AND RECORDED_DATE >= DATEADD('year', -2, CURRENT_DATE) 
+            THEN CODE_HCC END) AS RECENT_THIRD_PARTY_HCC_COUNT
     FROM
         CONDITION
     WHERE CODE_HCC IS NOT NULL
     GROUP BY
         UPID
 ),
--- Step 4: Count the new condition count per patient
-NEW_CONDITIONS_AGG AS (
+-- Step 4: Count the HCC opportunities per patient
+HCC_OPPORTUNITY_AGG AS (
     SELECT
         UPID,
-        COUNT(DISTINCT CODE_HCC) AS NEW_HCC_COUNT
+        COUNT(DISTINCT CODE_HCC) AS HCC_OPPORTUNITY_COUNT
     FROM
-        THIRD_PARTY_ONLY_HCCS
+        RECENT_THIRD_PARTY_ONLY_HCCS
     GROUP BY
         UPID
 )
--- Step 5: Combine the aggregated results with the net new HCC codes and list them
+-- Step 5: Combine the zaggregated results with the HCC opportunity codes and list them
 SELECT
     phc.UPID,
     CONCAT('https://app.zushealth.com/patients/',phc.UPID, '/conditions') AS ZusAppLink,
     phc.FIRST_PARTY_HCC_COUNT,
-    phc.THIRD_PARTY_HCC_COUNT,
-    COALESCE(nca.NEW_HCC_COUNT, 0) AS NEW_HCC_COUNT,
-    LISTAGG(DISTINCT tpo.CODE_HCC, ', ') WITHIN GROUP (ORDER BY tpo.CODE_HCC) AS NEW_HCC_CODES,
+    phc.RECENT_THIRD_PARTY_HCC_COUNT,
+    COALESCE(hca.HCC_OPPORTUNITY_COUNT, 0) AS HCC_OPPORTUNITY_COUNT,
+    LISTAGG(DISTINCT tpo.CODE_HCC, ', ') WITHIN GROUP (ORDER BY tpo.CODE_HCC) AS HCC_OPPORTUNITY_CODES,
 FROM
     PATIENT_HCC_COUNTS phc
 LEFT JOIN
-    NEW_CONDITIONS_AGG nca ON phc.UPID = nca.UPID
+    HCC_OPPORTUNITY_AGG hca ON phc.UPID = hca.UPID
 LEFT JOIN
-    THIRD_PARTY_ONLY_HCCS tpo ON phc.UPID = tpo.UPID
+    RECENT_THIRD_PARTY_ONLY_HCCS tpo ON phc.UPID = tpo.UPID
 GROUP BY
-    phc.UPID, phc.FIRST_PARTY_HCC_COUNT, phc.THIRD_PARTY_HCC_COUNT, nca.NEW_HCC_COUNT
+    phc.UPID, phc.FIRST_PARTY_HCC_COUNT, phc.RECENT_THIRD_PARTY_HCC_COUNT, hca.HCC_OPPORTUNITY_COUNT
 ORDER BY
     FIRST_PARTY_HCC_COUNT DESC NULLS LAST;
