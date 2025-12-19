58287a3b365c9a81acc4c583bc3afff04b9a4a79
Nick Luzarraga <93792086+NLuzarraga@users.noreply.github.com>
2025-12-19 10:35:24 -0600

    Update mapping_custom_identifier_to_upid.sql

--- 1.Understanding_Zus_Data/mapping_custom_identifier_to_upid.sql
+++ 1.Understanding_Zus_Data/mapping_custom_identifier_to_upid.sql
@@ -1,12 +1,10 @@
 -- Map from an organization's internal identifier (in this case with identifier system https://organizationName.com/ID) to Zus UPID
 SELECT
   PATIENT.UPID as zus_upid,
-  IDENTIFIER.VALUE as organization_patient_id
+  PATIENT_IDENTIFIER.VALUE as organization_patient_id
 FROM
   PATIENT
 JOIN PATIENT_IDENTIFIER
   ON PATIENT.ID = PATIENT_IDENTIFIER.PATIENT_ID
-JOIN IDENTIFIER
-  ON IDENTIFIER.ID = PATIENT_IDENTIFIER.IDENTIFIER_ID
 WHERE
-  IDENTIFIER.SYSTEM = 'https://organizationName.com/ID' ;
+  PATIENT_IDENTIFIER.SYSTEM = 'https://organizationName.com/ID' ;
